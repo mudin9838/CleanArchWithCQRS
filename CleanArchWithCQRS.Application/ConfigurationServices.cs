305a46deafe324d76999ab4bece55d912099868f
Muhdin <muhdinmussema@gmail.com>
2025-03-17 15:26:07 +0300

    Add project files.

---
+++ CleanArchWithCQRS.Application/ConfigurationServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchWithCQRS.Application
+{
+    public static class ConfigurationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(ctg => ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
+
---
+++ CleanArchWithCQRS.Infrastructure/ConfigueServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchWithCQRS.Infrastructure
+{
+    public static class ConfigueServices
+    {
+        public static IServiceCollection AddInfrasructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
